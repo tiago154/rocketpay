7ca3a16527ab05bf795a7a0218212eb7bebeb068
Tiago Silva <tiago.toya@hotmail.com>
2021-02-24 11:53:23 -0300

    ci: ajuste na variavel de ambiente do banco

--- config/test.exs
+++ config/test.exs
@@ -9,7 +9,7 @@ config :rocketpay, Rocketpay.Repo,
   username: "postgres",
   password: "postgres",
   database: "rocketpay_test#{System.get_env("MIX_TEST_PARTITION")}",
-  hostname: "localhost",
+  hostname: System.get_env("DB_HOST", "localhost"),
   pool: Ecto.Adapters.SQL.Sandbox
 
 # We don't run a server during test. If one is required,
